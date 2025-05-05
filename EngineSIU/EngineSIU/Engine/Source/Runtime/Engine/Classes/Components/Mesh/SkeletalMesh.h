0edeb87726031185747cbd919e62c919582da3f3
gudtldn <gudtldn1234@gmail.com>
2025-05-05 23:50:55 +0900

    feat: SkeletalMesh 및 StaticMesh 클래스 추가

---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Components/Mesh/SkeletalMesh.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "BaseMesh.h"
+#include "UObject/ObjectMacros.h"
+
+
+class USkeletalMesh : public UMeshAsset
+{
+    DECLARE_CLASS(USkeletalMesh, UMeshAsset)
+
+public:
+    USkeletalMesh() = default;
+    virtual ~USkeletalMesh() override = default;
+
+public:
+    TArray<FSkinnedVertex> Vertices;
+    TArray<uint32> Indices;
+    TArray<FMeshSubset> Subsets;
+
+    FSkeleton SkeletonData;
+    TArray<FAnimationClip> AnimationClips; // 감지된 애니메이션 클립 목록
+    bool bHasAnimationData = false; // 애니메이션 데이터 존재 유무 플래그
+};
---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Components/Mesh/StaticMesh.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "BaseMesh.h"
+#include "UObject/ObjectMacros.h"
+
+
+class UStaticMeshTest : public UMeshAsset
+{
+    DECLARE_CLASS(UStaticMeshTest, UMeshAsset)
+
+public:
+    UStaticMeshTest() = default;
+
+public:
+    TArray<FVertex> Vertices;
+    TArray<uint32> Indices; // 32비트 인덱스 버퍼 사용 가정
+    TArray<FMeshSubset> Subsets; // 머티리얼별 드로우 콜 정보
+};
