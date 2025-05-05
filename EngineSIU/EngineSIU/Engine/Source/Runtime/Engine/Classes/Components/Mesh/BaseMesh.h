a50aaab94ac2c086c12f00963400fd0d816cca30
gudtldn <gudtldn1234@gmail.com>
2025-05-05 23:50:43 +0900

    feat: 공통 메쉬 에셋 인터페이스를 위한 UMeshAsset 클래스 추가

---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Components/Mesh/BaseMesh.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "EngineMeshTypes.h"
+#include "Container/Array.h"
+#include "UObject/NameTypes.h"
+#include "UObject/ObjectMacros.h"
+
+
+// 공통 메쉬 에셋 인터페이스
+class UMeshAsset : public UObject
+{
+    DECLARE_CLASS(UMeshAsset, UObject)
+
+public:
+    UMeshAsset() = default;
+    virtual ~UMeshAsset() override = default;
+
+    FName AssetName;                 // 에셋 이름
+    TArray<FMaterialInfo> Materials; // 이 메쉬에서 사용하는 머티리얼 목록
+};
