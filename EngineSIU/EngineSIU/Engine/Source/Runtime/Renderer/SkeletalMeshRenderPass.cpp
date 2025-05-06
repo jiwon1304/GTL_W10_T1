98d5bae889c51d5b51d4c2782436b98138cb90cf
gudtldn <gudtldn1234@gmail.com>
2025-05-06 22:07:53 +0900

    feat: FSkeletalMeshRenderPass 클래스 구현 및 IRenderPass 인터페이스 메서드 추가

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Renderer/SkeletalMeshRenderPass.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Renderer/SkeletalMeshRenderPass.cpp
@@ -1,1 +1,23 @@
 ﻿#include "SkeletalMeshRenderPass.h"
+
+FSkeletalMeshRenderPass::FSkeletalMeshRenderPass()
+    : BufferManager(nullptr)
+    , Graphics(nullptr)
+{
+}
+
+void FSkeletalMeshRenderPass::Initialize(FDXDBufferManager* InBufferManager, FGraphicsDevice* InGraphics, FDXDShaderManager* InShaderManage)
+{
+}
+
+void FSkeletalMeshRenderPass::PrepareRenderArr()
+{
+}
+
+void FSkeletalMeshRenderPass::Render(const std::shared_ptr<FEditorViewportClient>& Viewport)
+{
+}
+
+void FSkeletalMeshRenderPass::ClearRenderArr()
+{
+}
--- EngineSIU/EngineSIU/Engine/Source/Runtime/Renderer/SkeletalMeshRenderPass.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Renderer/SkeletalMeshRenderPass.h
@@ -1,7 +1,33 @@
 ﻿#pragma once
+#include "IRenderPass.h"
+#include "Container/Array.h"
 
-class SkeletalMeshRenderPass
+class USkeletalMeshComponent;
+
+
+class FSkeletalMeshRenderPass : public IRenderPass
 {
 public:
-    
+    FSkeletalMeshRenderPass();
+    virtual ~FSkeletalMeshRenderPass() override = default;
+
+    // 이동 & 복사 생성자 제거
+    FSkeletalMeshRenderPass(const FSkeletalMeshRenderPass&) = delete;
+    FSkeletalMeshRenderPass& operator=(const FSkeletalMeshRenderPass&) = delete;
+    FSkeletalMeshRenderPass(FSkeletalMeshRenderPass&&) = delete;
+    FSkeletalMeshRenderPass& operator=(FSkeletalMeshRenderPass&&) = delete;
+
+public:
+    // IRenderPass
+    virtual void Initialize(FDXDBufferManager* InBufferManager, FGraphicsDevice* InGraphics, FDXDShaderManager* InShaderManage) override;
+    virtual void PrepareRenderArr() override;
+    virtual void Render(const std::shared_ptr<FEditorViewportClient>& Viewport) override;
+    virtual void ClearRenderArr() override;
+    // ~IRenderPass
+
+protected:
+    TArray<USkeletalMeshComponent*> StaticMeshComponents;
+
+    FDXDBufferManager* BufferManager;
+    FGraphicsDevice* Graphics;
 };
