91ae6835f448fce9571e8a4789130a2da714f943
gudtldn <gudtldn1234@gmail.com>
2025-05-06 16:18:44 +0900

    feat: SerializeMeshAsset 클래스 추가 및 이진 파일에서 StaticMesh 및 SkeletalMesh 로드/저장 기능 구현

---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Core/Serialization/SerializeMeshAsset.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include "Container/Array.h"
+
+class UMeshAsset;
+class UStaticMeshTest;
+class USkeletalMesh;
+
+
+struct FSerializeMeshAsset
+{
+public:
+    static constexpr uint32 Version = 1;
+
+    static UStaticMeshTest* LoadStaticMeshFromBinary(const FString& FilePath);
+    static USkeletalMesh* LoadSkeletalMeshFromBinary(const FString& FilePath);
+
+    static bool SaveStaticMeshToBinary(const FString& FilePath, UStaticMeshTest* StaticMesh);
+    static bool SaveSkeletalMeshToBinary(const FString& FilePath, USkeletalMesh* SkeletalMesh);
+
+private:
+    static FArchive& SerializeVersion(FArchive& Ar);
+    static FArchive& SerializeMeshAssetBase(FArchive& Ar, UMeshAsset* Mesh);
+    static FArchive& SerializeMeshAsset(FArchive& Ar, UStaticMeshTest* StaticMesh);
+    static FArchive& SerializeMeshAsset(FArchive& Ar, USkeletalMesh* SkeletalMesh);
+};
