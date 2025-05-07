64c5af7feb20d04e9d60849ae5a5ef2b261c897b
gudtldn <gudtldn1234@gmail.com>
2025-05-08 04:07:54 +0900

    feat: FTransform 구조체 추가 및 Rotator 복사 생성자 제거

---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Core/Math/Transform.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+#include "MathUtility.h"
+#include "Rotator.h"
+#include "Vector.h"
+
+/**
+ * 위치, 회전, 스케일 정보를 포함하는 3D 변환(Transform) 구조체입니다.
+ *
+ * 이 구조체는 3D 공간에서의 변환을 표현하는 데 사용되며, 다음 요소들을 포함합니다:
+ * - 위치(Translation): 3D 공간에서 객체의 이동
+ * - 회전(Rotation): 객체의 방향(보통 오일러 각도로 정의)
+ * - 스케일(Scale): X, Y, Z 축을 따라 객체의 크기 조정
+ *
+ * FTransform 클래스는 변환을 항등 상태(Identity)로 초기화하거나, 주어진 행렬로부터 초기화할 수 있는 생성자를 제공합니다.
+ */
+struct FTransform
+{
+public:
+    FTransform();
+    FTransform(const FMatrix& InMatrix);
+
+public:
+    FVector GetTranslation() const { return Translation; }
+    void SetTranslation(const FVector& InTranslation) { Translation = InTranslation; }
+
+    FRotator GetRotation() const { return Rotation; }
+    void SetRotation(const FRotator& InRotation) { Rotation = InRotation; }
+
+    FVector GetScale3D() const { return Scale3D; }
+    void SetScale3D(const FVector& InScale3D) { Scale3D = InScale3D; }
+
+protected:
+    FVector Translation;
+    FRotator Rotation;
+    FVector Scale3D;
+};
--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Engine/Asset/SkeletalMeshAsset.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Engine/Asset/SkeletalMeshAsset.h
@@ -4,43 +4,19 @@
 #include "Container/Array.h"
 #include "Container/Map.h"
 #include "Math/Vector.h"
-#include "Math/Matrix.h"
 #include "Math/Quat.h"
 #include "Math/Color.h"
+#include "Math/Transform.h"
 
 struct FMeshBoneInfo
 {
     FString Name;
     int32 ParentIndex;
-};
 
-// 단순히 TRS를 담는 구조체입니다.
-// 나중에 옮겨주세요
-struct FTransform
-{
-    FVector Translation;
-    FRotator Rotation;
-    FVector Scale3D;
-    FTransform() : Translation(FVector::ZeroVector), Rotation(FRotator::ZeroRotator), Scale3D(FVector::OneVector) {}
-
-    void SetFromMatrix(const FMatrix& InMatrix)
+public:
+    friend FArchive& operator<<(FArchive& Ar, FMeshBoneInfo& Data)
     {
-        // Extract translation
-        Translation = InMatrix.GetTranslationVector();
-
-        // Extract rotation and scale from the matrix
-        FMatrix RotationMatrix = InMatrix;
-        Scale3D = RotationMatrix.ExtractScaling(SMALL_NUMBER);
-
-        // If there is negative scaling, handle it
-        if (InMatrix.Determinant() < 0.0f)
-        {
-            // Reflect matrix to ensure proper handedness
-            Scale3D *= -1.0f;
-            RotationMatrix.SetAxis(0, -RotationMatrix.GetScaledAxis(0));
-        }
-
-        Rotation = RotationMatrix.Rotator();
+        return Ar << Data.Name << Data.ParentIndex;
     }
 };
 
