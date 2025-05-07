d63a79cedb0c8e92f254303f4498d1383650e2cb
gudtldn <gudtldn1234@gmail.com>
2025-05-07 23:07:43 +0900

    feat: BaseMesh.cpp 파일 추가 및 UpdateMaterials 메서드 구현

---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Components/Mesh/BaseMesh.cpp
@@ -0,0 +1,14 @@
+﻿#include "BaseMesh.h"
+#include "Engine/FObjLoader.h"
+
+
+void UMeshAsset::UpdateMaterials()
+{
+    Materials.Empty(MaterialInfos.Num());
+
+    for (const FMaterialInfo& MatInfo : MaterialInfos)
+    {
+        // TODO: 아니 왜 MaterialMap이 ResourceManager가 아니라 FObjManager에 있지?
+        Materials.Add(FObjManager::CreateMaterial(MatInfo.ConvertObjMaterialInfo()));
+    }
+}
--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Components/Mesh/BaseMesh.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Components/Mesh/BaseMesh.h
@@ -15,6 +15,9 @@ public:
     UMeshAsset() = default;
     virtual ~UMeshAsset() override = default;
 
-    FAssetInfo Info;                 // Asset의 정보
-    TArray<FMaterialInfo> Materials; // 이 메쉬에서 사용하는 머티리얼 목록
+    void UpdateMaterials();
+
+    FAssetInfo Info;                     // Asset의 정보
+    TArray<UMaterial*> Materials;        // 이 메쉬에서 사용하는 머티리얼 목록
+    TArray<FMaterialInfo> MaterialInfos; // FBX에서 불러와진 Raw Data
 };
