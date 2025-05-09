a194390b8ca687c26e51a48c7f15731e980d7d42
jiwon1304 <143168061+jiwon1304@users.noreply.github.com>
2025-05-10 00:36:47 +0900

    fix : merge 중 오류 수정

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Engine/FFbxLoader.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Engine/FFbxLoader.h
@@ -77,9 +77,3 @@ struct FFbxManager
     static bool SaveFBXToBinary(const FWString& FilePath, int64_t LastModifiedTime, const FFbxSkeletalMesh& FBXObject);
     static bool LoadFBXFromBinary(const FWString& FilePath, int64_t LastModifiedTime, FFbxSkeletalMesh& OutFBXObject);
 };
-
-struct FFbxManager
-{
-    static bool SaveFBXToBinary(const FWString& FilePath, int64_t LastModifiedTime, const FFbxSkeletalMesh& FBXObject);
-    static bool LoadFBXFromBinary(const FWString& FilePath, int64_t LastModifiedTime, FFbxSkeletalMesh& OutFBXObject);
-};
